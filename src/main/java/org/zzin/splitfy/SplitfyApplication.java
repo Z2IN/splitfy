5e12014fe9d7aa178ef0eddf9e7728ef197bda54
abko <abko121115@gmail.com>
2025-12-01 02:05:24 +0000

    init: 프로젝트 초기화

---
+++ src/main/java/org/zzin/splitfy/SplitfyApplication.java
@@ -0,0 +1,13 @@
+package org.zzin.splitfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitfyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zzin/splitfy/SplitfyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zzin.splitfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
