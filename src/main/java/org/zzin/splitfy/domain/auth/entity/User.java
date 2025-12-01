c511971d7db32973ccb4158b78c560d4b559ab4a
eunjin0468 <eunjin0468@naver.com>
2025-12-01 13:55:32 +0900

    feat: domain/user 패키지 구성

--- src/main/java/org/zzin/splitfy/domain/auth/entity/User.java
+++ src/main/java/org/zzin/splitfy/domain/auth/entity/User.java
@@ -5,15 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
