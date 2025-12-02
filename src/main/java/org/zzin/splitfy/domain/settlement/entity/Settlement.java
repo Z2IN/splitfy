7bbb902217919c23a2d5cc7f0194ae7103f05708
dxxwls <75723636+eunjin0468@users.noreply.github.com>
2025-12-02 10:31:19 +0900

    chore: domain/settlement 패키지 구성

---
+++ src/main/java/org/zzin/splitfy/domain/settlement/entity/Settlement.java
@@ -0,0 +1,44 @@
+package org.zzin.splitfy.domain.settlement.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.zzin.splitfy.domain.settlement.enums.SettlementStatus;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "settlements")
+public class Settlement {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private Long issuerId;
+
+  @Column(nullable = false)
+  private Long totalAmount;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private SettlementStatus status;
+
+  @CreatedDate
+  @Column(nullable = false, updatable = false)
+  private LocalDateTime issuedAt;
+
+  @Column
+  private LocalDateTime succeededAt;
+}
