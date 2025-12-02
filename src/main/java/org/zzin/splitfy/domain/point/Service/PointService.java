49633783168e3c0e083dec6ff25e876b9c6d9a28
abko <abko121115@gmail.com>
2025-12-02 04:36:22 +0000

    docs: PointService에 getPointBy 메서드에 대한 주석 추가

--- src/main/java/org/zzin/splitfy/domain/point/Service/PointService.java
+++ src/main/java/org/zzin/splitfy/domain/point/Service/PointService.java
@@ -11,7 +11,14 @@ public class PointService {
 
   private final AuthInnerService authInnerService;
 
+  /**
+   * 주어진 사용자 ID에 대한 포인트 잔액을 조회하여 반환합니다.
+   *
+   * @param userId 조회할 사용자의 고유 식별자
+   * @return 요청한 사용자의 현재 포인트 잔액
+   */
   public long getPointBy(long userId) {
     return authInnerService.getPointBy(userId);
   }
+
 }
