aa81d787513c9d947c3e036f2082502361ac079e
dxxwls <75723636+eunjin0468@users.noreply.github.com>
2025-12-02 10:29:44 +0900

    chore: domain/auth 패키지 구성

---
+++ src/main/java/org/zzin/splitfy/domain/auth/controller/AuthController.java
@@ -0,0 +1,4 @@
+package org.zzin.splitfy.domain.auth.controller;
+
+public class AuthController {
+}
---
+++ src/main/java/org/zzin/splitfy/domain/auth/entity/User.java
@@ -0,0 +1,35 @@
+package org.zzin.splitfy.domain.auth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "users")
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private Long point = 0L;
+
+  @Column(nullable = false, unique = true)
+  private String username;
+
+}
