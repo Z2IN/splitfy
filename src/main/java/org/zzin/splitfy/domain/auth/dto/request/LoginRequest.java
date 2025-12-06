6b9a27a50272a7acfe5f1e9d974620fe4a13a45a
eunjin0468 <eunjin0468@naver.com>
2025-12-06 11:17:10 +0900

    refactor: 요청 DTO에서 불필요한 @JsonProperty(WRITE_ONLY) 제거

--- src/main/java/org/zzin/splitfy/domain/auth/dto/request/LoginRequest.java
+++ src/main/java/org/zzin/splitfy/domain/auth/dto/request/LoginRequest.java
@@ -1,6 +1,5 @@
 package org.zzin.splitfy.domain.auth.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
@@ -9,7 +8,6 @@ public record LoginRequest(
     @NotBlank
     String email,
     @NotBlank
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     String password
 ) {
 
