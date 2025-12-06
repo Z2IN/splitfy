5672c7b6286fa88dd6e1498937a28b7e916c3a85
eunjin0468 <eunjin0468@naver.com>
2025-12-07 01:20:15 +0900

    refactor: Login/Signup Request DTO를 record에서 class로 변경하여 password toString 노출 방지

--- src/main/java/org/zzin/splitfy/domain/auth/dto/request/LoginRequest.java
+++ src/main/java/org/zzin/splitfy/domain/auth/dto/request/LoginRequest.java
@@ -2,13 +2,16 @@ package org.zzin.splitfy.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public record LoginRequest(
-    @Email
-    @NotBlank
-    String email,
-    @NotBlank
-    String password
-) {
+@Getter
+@NoArgsConstructor
+public class LoginRequest {
 
+  @Email
+  @NotBlank
+  private String email;
+  @NotBlank
+  private String password;
 }
