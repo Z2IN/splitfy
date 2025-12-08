825008c916c737e837abaf11e000a03fec009204
soo59599 <sooh59599@gmail.com>
2025-12-08 14:41:14 +0900

    feat: EventNumber 관련 도메인 작성

---
+++ src/main/java/org/zzin/splitfy/domain/event/service/DefaultEventNumberInnerService.java
@@ -0,0 +1,8 @@
+package org.zzin.splitfy.domain.event.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultEventNumberInnerService implements EventNumberInnerService {
+
+}
