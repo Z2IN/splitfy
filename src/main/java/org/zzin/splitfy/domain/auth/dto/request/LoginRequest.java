389e7619c45871eb0571841b7f644b9cd16424a3
dxxwls <eunjin0468@naver.com>
2025-12-08 11:05:44 +0900

    refactor: Login/Signup Request DTO를 class에서 record로 변경

--- src/main/java/org/zzin/splitfy/domain/auth/dto/request/LoginRequest.java
+++ src/main/java/org/zzin/splitfy/domain/auth/dto/request/LoginRequest.java
@@ -2,16 +2,9 @@ package org.zzin.splitfy.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
-public class LoginRequest {
+public record LoginRequest(
 
-  @Email
-  @NotBlank
-  private String email;
-  @NotBlank
-  private String password;
+    @Email @NotBlank String email, @NotBlank String password) {
+
 }
