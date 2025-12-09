6503986913cbb46fdca125bdfd0417ba01b8182b
dxxwls <eunjin0468@naver.com>
2025-12-09 15:12:18 +0900

    refactor: @NullMarked 추가

--- src/main/java/org/zzin/splitfy/domain/auth/service/AuthInnerService.java
+++ src/main/java/org/zzin/splitfy/domain/auth/service/AuthInnerService.java
@@ -1,9 +1,11 @@
 package org.zzin.splitfy.domain.auth.service;
 
 import lombok.NonNull;
+import org.jspecify.annotations.NullMarked;
 import org.zzin.splitfy.domain.auth.dto.PointChangeResultDTO;
 import org.zzin.splitfy.domain.auth.dto.PointTransferSummaryDTO;
 
+@NullMarked
 public interface AuthInnerService {
 
   long getPointBy(long userId);
