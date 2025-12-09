99f5b92f2fcb0c3285271b720e955d6af4c26f5b
abko <abko121115@gmail.com>
2025-12-09 05:13:55 +0000

    refactor: PointController에서 불필요한 import 제거

--- src/main/java/org/zzin/splitfy/domain/point/controller/PointController.java
+++ src/main/java/org/zzin/splitfy/domain/point/controller/PointController.java
@@ -16,7 +16,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
