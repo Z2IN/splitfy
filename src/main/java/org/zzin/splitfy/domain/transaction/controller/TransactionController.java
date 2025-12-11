1bbe1c5ad388d6d1d5e8dbfd9503109a091e724a
abko <abko121115@gmail.com>
2025-12-11 02:11:13 +0000

    refactor: 불필요한 import 문 제거

--- src/main/java/org/zzin/splitfy/domain/transaction/controller/TransactionController.java
+++ src/main/java/org/zzin/splitfy/domain/transaction/controller/TransactionController.java
@@ -8,8 +8,6 @@ import org.zzin.splitfy.domain.transaction.service.TransactionService;
 
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
