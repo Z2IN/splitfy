13080e7e8b56e312203a8674acd91f36e3a31271
abko <abko121115@gmail.com>
2025-12-11 04:26:28 +0000

    refactor: 잘못된 import 문 수정

--- src/main/java/org/zzin/splitfy/domain/transaction/dto/TransactionDetailDTO.java
+++ src/main/java/org/zzin/splitfy/domain/transaction/dto/TransactionDetailDTO.java
@@ -5,9 +5,8 @@ import java.time.LocalDateTime;
 import org.jspecify.annotations.NullMarked;
 import org.zzin.splitfy.domain.transaction.enums.TransactionType;
 
-import com.google.auto.value.AutoValue.Builder;
-
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Builder
