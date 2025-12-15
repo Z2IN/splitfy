88fb6f40d1f516e04a30b1d51acde8add7e14b48
abko <abko121115@gmail.com>
2025-12-15 04:24:29 +0000

    feat: 거래 내역 조회 API의 응답 형식을 CommonResponse로 변경

--- src/main/java/org/zzin/splitfy/domain/transaction/controller/TransactionController.java
+++ src/main/java/org/zzin/splitfy/domain/transaction/controller/TransactionController.java
@@ -2,6 +2,7 @@ package org.zzin.splitfy.domain.transaction.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.zzin.splitfy.common.dto.CommonPage;
+import org.zzin.splitfy.common.dto.CommonResponse;
 import org.zzin.splitfy.common.security.AuthUser;
 import org.zzin.splitfy.domain.transaction.dto.response.GetTransactionsByResponse;
 import org.zzin.splitfy.domain.transaction.service.TransactionService;
@@ -25,14 +26,14 @@ public class TransactionController {
   private final TransactionService transactionService;
 
   @GetMapping("/transactions")
-  public CommonPage<GetTransactionsByResponse> getTransactionsBy(
+  public CommonResponse<CommonPage<GetTransactionsByResponse>> getTransactionsBy(
       @AuthenticationPrincipal AuthUser authUser, @PageableDefault Pageable pageable) {
 
     var pageResult = transactionService.getTransactionsBy(
         authUser, pageable.getPageNumber(), pageable.getPageSize());
     Page<GetTransactionsByResponse> response = pageResult.map(GetTransactionsByResponse::fromDto);
 
-    return CommonPage.of(response);
+    return CommonResponse.success(CommonPage.of(response));
   }
 
 }
