03cfc30de35ba77788d0d0849c239dee1ee8fdcf
dxxwls <eunjin0468@naver.com>
2025-12-18 15:29:17 +0900

    refactor: PointInnerService 인터페이스 및 구현체 추가

--- src/main/java/org/zzin/splitfy/domain/point/Service/DefaultPointInnerService.java
+++ src/main/java/org/zzin/splitfy/domain/point/Service/DefaultPointInnerService.java
@@ -23,4 +23,17 @@ public class DefaultPointInnerService implements PointInnerService {
     return userPointRepository.save(userPoint).getPoint();
   }
 
+  /**
+   * 송신자로부터 수신자에게 포인트를 이체합니다. (자동 이체용)
+   *
+   * @param fromUserId 송신자의 사용자 ID
+   * @param toUserId   수신자의 사용자 ID
+   * @param amount     이체할 포인트 양 (양수여야 함)
+   */
+  @Override
+  @Transactional
+  public void transferPoint(long fromUserId, long toUserId, long amount) {
+    // TODO: PointService.transferTo 로직을 참고하여 구현
+  }
+
 }
