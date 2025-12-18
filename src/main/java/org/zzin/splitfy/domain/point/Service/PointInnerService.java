b9d4d442f11f1763e87f4fabf425ec960be5be4f
abko <abko121115@gmail.com>
2025-12-18 12:04:15 +0000

    docs: PointInnerService에서 불필요한 주석 제거

--- src/main/java/org/zzin/splitfy/domain/point/Service/PointInnerService.java
+++ src/main/java/org/zzin/splitfy/domain/point/Service/PointInnerService.java
@@ -4,8 +4,6 @@ public interface PointInnerService {
 
   long initUserPoint(long userId);
 
-  // TODO: eventId + 운영자 가 userId 에게 reward를 보내면 됩니다
-  // 트랜잭션에도 저장해야할까요?
   void sendEventRewardPoint(long userId, long reward);
 
 }
