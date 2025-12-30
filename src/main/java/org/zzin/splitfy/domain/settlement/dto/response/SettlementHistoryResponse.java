6097c12dc180d068c38fa9a435b5952c1eed66fa
dxxwls <eunjin0468@naver.com>
2025-12-30 20:07:26 +0900

    refactor:  setId() 메서드 추가 및 primitive타입으로 변경

--- src/main/java/org/zzin/splitfy/domain/settlement/dto/response/SettlementHistoryResponse.java
+++ src/main/java/org/zzin/splitfy/domain/settlement/dto/response/SettlementHistoryResponse.java
@@ -5,12 +5,12 @@ import java.util.List;
 import org.zzin.splitfy.domain.settlement.enums.SettlementStatus;
 
 public record SettlementHistoryResponse(
-    Long settlementId,
-    Long totalPaidAmount,
+    long settlementId,
+    long totalPaidAmount,
     SettlementStatus status,
     LocalDateTime issuedAt,
     LocalDateTime succeededAt,
-    Long settlementAmount,
+    long settlementAmount,
     List<SettlementPaymentResponse> payments
 ) {
 
--- src/main/java/org/zzin/splitfy/domain/settlement/entity/Payment.java
+++ src/main/java/org/zzin/splitfy/domain/settlement/entity/Payment.java
@@ -47,4 +47,7 @@ public class Payment {
     this.settlementId = settlementId;
   }
 
+  public void setId(Long id) {
+    this.id = id;
+  }
 }
